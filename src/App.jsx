2924584a5620f017853c2fb37cf1ff3f15421698
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-07-18 00:51:17 +0600

    done @ footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Loading from "./components/Loading";
 
 function App() {
   return (
     <>
       <Loading />
+      <Footer />
     </>
   );
 }
