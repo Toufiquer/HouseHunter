b4ec9c62a16b3c3b36569aec82fce7dacd4df891
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-07-18 16:51:41 +0600

    done @ handle error user dublicate

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isAuth, setIsAuth] = React.useState(false);
+  
   const menuItems = (
     <>
       <li>
@@ -51,39 +54,43 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1">{menuItems}</ul>
         </div>
         <div className="navbar-end ">
-          <div className="flex gap-2">
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="Search"
-                className="input input-bordered w-24 md:w-auto"
-              />
+          {isAuth ? (
+            <div className="flex gap-2">
+              <div className="form-control">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="input input-bordered w-24 md:w-auto"
+                />
+              </div>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <a className="justify-between">
+                      Profile
+                      <span className="badge">New</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>Settings</a>
+                  </li>
+                  <li>
+                    <a>Logout</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+          ) : (
+            <Link to="/logIn">LogIn</Link>
+          )}
         </div>
       </div>
     </div>
--- src/pages/LogInRegistration/Registration.jsx
+++ src/pages/LogInRegistration/Registration.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { useAddUserMutation } from "../../redux/features/users/usersApi";
+import Loading from "../../components/Loading/Loading";
+import DisplayCenter from "../../components/DisplayCenter/DisplayCenter";
 
 const Registration = () => {
+  const [addUser, { isSuccess, isLoading, isError, error, data }] =
+    useAddUserMutation();
+  console.log(isSuccess, error);
   const options = [
     { value: "house_Owner", text: "House Owner" },
     { value: "house_Renter", text: "House Renter" },
@@ -12,110 +18,138 @@ const Registration = () => {
   const email = React.useRef();
   const password = React.useRef();
   const conPassword = React.useRef();
-
   const [role, setRole] = React.useState(options[0].value);
   const [isAgree, setIsAgree] = React.useState(true);
 
   const handleChange = (event) => {
-    console.log(event.target.value);
     setRole(event.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Name:", name.current?.value);
-    console.log("role:", role);
-    console.log("phone:", phone.current?.value);
-    console.log("email:", email.current?.value);
-    console.log("password:", password.current?.value);
-    console.log("conPassword:", conPassword.current?.value);
+
+    if (
+      name.current?.value &&
+      phone.current?.value &&
+      email.current?.value &&
+      password.current?.value &&
+      conPassword.current?.value &&
+      role
+    ) {
+      const data = {
+        name: name.current.value,
+        phone: phone.current.value,
+        email: email.current.value,
+        password: password.current.value,
+        role,
+      };
+      addUser(data);
+    }
   };
-  return (
-    <div>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2 ">
-        {" "}
-        <label className="label">
-          <span className="label-text">What is your Name?</span>
-        </label>
-        <input
-          type="text"
-          ref={name}
-          placeholder="Name"
-          className="input input-bordered input-primary w-[400px]"
-        />{" "}
-        <label className="label">
-          <span className="label-text">Who are You?</span>
-        </label>
-        <select
-          className="input input-bordered input-primary w-[400px]"
-          value={role}
-          onChange={handleChange}
-        >
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.text}
-            </option>
-          ))}
-        </select>{" "}
-        <label className="label">
-          <span className="label-text">What is your Phone?</span>
-        </label>
-        <input
-          type="phone"
-          ref={phone}
-          placeholder="Phone"
-          className="input input-bordered input-primary w-[400px]"
-        />{" "}
-        <label className="label">
-          <span className="label-text">What is your Email?</span>
-        </label>
-        <input
-          type="email"
-          ref={email}
-          placeholder="Email"
-          className="input input-bordered input-primary w-[400px]"
-        />{" "}
-        <label className="label">
-          <span className="label-text">What is your Password?</span>
-        </label>
-        <input
-          type="password"
-          ref={password}
-          placeholder="Password"
-          className="input input-bordered input-primary w-[400px]"
-        />
-        <label className="label">
-          <span className="label-text">Confirm Password?</span>
-        </label>
-        <input
-          type="password"
-          ref={conPassword}
-          placeholder="Password"
-          className="input input-bordered input-primary w-[400px]"
-        />
-        <div className="flex items-center ">
+
+  // decide what to render
+  let content;
+  if (isLoading && !isError) {
+    content = <Loading />;
+  } else if (!isLoading && isError) {
+    content = <DisplayCenter>Ops! Some thing went wrong</DisplayCenter>;
+  } else if (isSuccess) {
+    console.log(data);
+    if (data.isError) {
+      content = <DisplayCenter>{data.message}</DisplayCenter>;
+    } else {
+      return <Navigate to="/" />;
+    }
+  } else {
+    content = (
+      <div>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 ">
+          {" "}
+          <label className="label">
+            <span className="label-text">What is your Name?</span>
+          </label>
           <input
-            type="radio"
-            name="radio-2"
-            className="radio radio-primary"
-            checked={isAgree}
-            onClick={() => setIsAgree((pre) => !pre)}
+            type="text"
+            ref={name}
+            placeholder="Name"
+            className="input input-bordered input-primary w-[400px]"
+          />{" "}
+          <label className="label">
+            <span className="label-text">Who are You?</span>
+          </label>
+          <select
+            className="input input-bordered input-primary w-[400px]"
+            value={role}
+            onChange={handleChange}
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.text}
+              </option>
+            ))}
+          </select>{" "}
+          <label className="label">
+            <span className="label-text">What is your Phone?</span>
+          </label>
+          <input
+            type="text"
+            ref={phone}
+            placeholder="Phone"
+            className="input input-bordered input-primary w-[400px]"
+          />{" "}
+          <label className="label">
+            <span className="label-text">What is your Email?</span>
+          </label>
+          <input
+            type="email"
+            ref={email}
+            placeholder="Email"
+            className="input input-bordered input-primary w-[400px]"
+          />{" "}
+          <label className="label">
+            <span className="label-text">What is your Password?</span>
+          </label>
+          <input
+            type="password"
+            ref={password}
+            placeholder="Password"
+            className="input input-bordered input-primary w-[400px]"
           />
           <label className="label">
-            <span className="label-text">
-              Agree with <Link className="underline">trams</Link> and{" "}
-              <Link className="underline">condition</Link>
-            </span>
+            <span className="label-text">Confirm Password?</span>
           </label>
-        </div>
-        <input
-          type="submit"
-          value="Submit"
-          className="btn btn-outline btn-primary input input-bordered input-primary w-[400px]"
-        />
-      </form>
-    </div>
-  );
+          <input
+            type="password"
+            ref={conPassword}
+            placeholder="Password"
+            className="input input-bordered input-primary w-[400px]"
+          />
+          <div className="flex items-center ">
+            <input
+              readOnly
+              type="radio"
+              name="radio-2"
+              className="radio radio-primary"
+              checked={isAgree}
+              onClick={() => setIsAgree((pre) => !pre)}
+            />
+            <label className="label">
+              <span className="label-text">
+                Agree with <Link className="underline">trams</Link> and{" "}
+                <Link className="underline">condition</Link>
+              </span>
+            </label>
+          </div>
+          <input
+            type="submit"
+            value="Submit"
+            className="btn btn-outline btn-primary input input-bordered input-primary w-[400px]"
+          />
+        </form>
+      </div>
+    );
+  }
+  return content;
 };
 
 export default Registration;
